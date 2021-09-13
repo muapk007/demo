6bd0688a04cb9f8a2b7e1a3b1ede5b3a6e062a81
Muhammad Usman Anwar <78350179+muapk007@users.noreply.github.com>
2021-09-13 18:32:03 +0300

    Magdi-e-invoice

---
+++ e_invoice/__manifest__.py
@@ -0,0 +1,14 @@
+{
+  'name': 'E-Invoice',
+  'description': 'This module for e-invoice',
+  'version': '13.0',
+  'author': 'Telenoc',
+  'depends': ['account'],
+  'data': [
+      'views/account_move_view_changes.xml',
+      'views/e_invoice_account_move.xml',
+       # 'views/new5rtl.xml',
+      'views/assets.xml'
+  ],
+
+}

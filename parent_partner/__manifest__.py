b99ee74d3c3a99c7b6fa181875b5c35f4f5f0f24
Muhammad Usman Anwar <78350179+muapk007@users.noreply.github.com>
2021-09-25 18:10:17 +0300

    parent

---
+++ parent_partner/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Parent Partner",
+
+    'summary': """
+        Make A Field on Partner Form and Show Under The Address Type""",
+
+    'description': """
+        Make A Field on Partner Form and Show Under The Address Type
+    """,
+
+    'author': "Viltco",
+    'website': "http://www.viltco.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'contacts',
+    'version': '14.0.0.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'contacts', 'account'],
+
+    # always loaded
+    'data': [
+        'views/parent_partner_views.xml',
+    ],
+
+}
